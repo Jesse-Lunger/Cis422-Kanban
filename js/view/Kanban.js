ae14165c993ff6e5c489f0368112b70ca59a2374
eavila6 <ethan.alt128@gmail.com>
2022-05-25 16:21:10 -0700

    experimenting w dock, initlist, item, & kanban.js

--- js/view/Kanban.js
+++ js/view/Kanban.js
@@ -7,6 +7,8 @@ export default class Kanban {
 	constructor(root) {
 		this.root = root;
 
+		//these consts are giving their respective .js files
+		//the id and title required 
 		const boardingView = new Boarding(1, "Boarding");
 		const dockview = new Dock(2, "Dock");
 		const initView = new InitList(3, "InitList");
